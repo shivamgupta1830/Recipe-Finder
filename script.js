6ec8ba1474163568c06c60efed6699d97f5e8d63
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-01-31 18:07:08 +0530

    responsive UI

---
+++ script.js
@@ -0,0 +1,10 @@
+const searchBtn = document.querySelector(".search-btn");
+const searchText = document.querySelector(".search-text");
+const indian = document.querySelector(".indian");
+const chinese = document.querySelector(".chinese");
+const continental = document.querySelector(".continental");
+const italian = document.querySelector(".italian");
+
+searchBtn.addEventListener("click", () => {});
+
+const searchRecipe = () => {};
