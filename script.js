cc374fe919549163ce7a0c9f1825929c7bb15799
shivamgupta1830 <shivamgupta1830@gmail.com>
2024-02-02 23:13:25 +0530

    Responsive UI

--- script.js
+++ script.js
@@ -7,76 +7,19 @@ const content = document.querySelector(".recipe-details-content");
 
 const indian = document.querySelector(".indian");
 const chinese = document.querySelector(".chinese");
-const italian = document.querySelector(".italian");
+const italian = document.querySelector(".american");
 const indianLink = document.querySelector(".indian-link");
 const links = document.querySelectorAll(".link");
 const navlist = document.querySelector(".nav-list");
 const nav = document.getElementById("nav");
 const arrow = document.querySelector(".arrow");
 
-const searchRecipes = async (query) => {
-  container.innerHTML = `
-  <div class="loader"><h2>Searching Recipes....</h2>
-  <div><img src="./load.gif" width = "152px"></div></div>`;
-
-  const data = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
-  );
-  const response = await data.json();
-  console.log(response.meals);
-
-  container.innerHTML = "";
-  response.meals.forEach((meal) => {
-    const card = document.createElement("div");
-    card.classList.add("recipe-card");
-
-    card.innerHTML = `
-    <img src="${meal.strMealThumb}" alt="${meal.strArea}" />
-    <h3>${meal.strMeal}</h3>
-    <h5><span>${meal.strArea}</span> Dish</h5>
-    <h5>Belongs to <span>${meal.strCategory}</span> Category</h5>`;
-
-    container.appendChild(card);
-
-    const viewButton = document.createElement("button");
-
-    viewButton.innerText = "View recipe";
-    card.appendChild(viewButton);
-
-    viewButton.addEventListener("click", () => {
-      details.style.display = "block";
-      container.style.filter = "blur(3px)";
-
-      const youtubeId = meal.strYoutube.slice(32, meal.strYoutube.length);
-
-      content.innerHTML = `
-
-      <h2>${meal.strMeal}</h2>
-      <h3>Ingredients</h3>
-      <ul><li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li>
-      <li>abcdefgkfi</li></ul>
-      <h3>Instructions</h3>
-      <p>${meal.strInstructions}</p>
-      <h3>Video Instructions</h3>
-      
-      <iframe height="250" width="350"
-            src="https://www.youtube.com/embed/${youtubeId}">
-      </iframe>`;
-    });
-  });
-};
-
 searchBtn.addEventListener("click", () => {
   const searchInput = searchText.value.trim();
-  console.log(searchInput);
+  if (!searchInput) {
+    container.innerHTML = `<h2>Type the <span>meal</span> in the search box !!</h2>`;
+    return;
+  }
 
   searchRecipes(searchInput);
 });
@@ -86,7 +29,7 @@ closeBtn.addEventListener("click", () => {
   container.style.filter = "none";
 });
 
-// Area-wise   #############################################################
+// Area-wise serach card functionality #############################################################
 
 links.forEach((link) => {
   const areaName = link.textContent;
@@ -97,14 +40,62 @@ links.forEach((link) => {
     <div><img src="./load.gif" width = "152px"></div></div>
   
   `;
-    const areaData = await fetch(
-      `https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaName}`
+
+    try {
+      const areaData = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaName}`
+      );
+      const areaResponse = await areaData.json();
+
+      container.innerHTML = "";
+
+      areaResponse.meals.forEach((meal) => {
+        const card = document.createElement("div");
+        card.classList.add("recipe-card");
+
+        card.innerHTML = `
+        <img src="${meal.strMealThumb}" alt="${meal.strArea}" />
+        <h3>${meal.strMeal}</h3>
+        <h5><span>${meal.strArea}</span> Dish</h5>
+        <h5>Belongs to <span>${meal.strCategory}</span> Category</h5>`;
+
+        container.appendChild(card);
+
+        const viewButton = document.createElement("button");
+
+        viewButton.innerText = "View recipe";
+        card.appendChild(viewButton);
+
+        viewButton.addEventListener("click", () => {
+          details.style.display = "block";
+          container.style.filter = "blur(3px)";
+
+          const mealId = meal.idMeal;
+
+          idDataLookup(mealId);
+        });
+      });
+    } catch (error) {
+      container.innerHTML = `<div class="error"><h2> Error in fetching recipes...</h2> <img src="./error.png"  alt="404-image" width="50%"/></div>`;
+    }
+  });
+});
+
+//  search recipe function################################
+
+const searchRecipes = async (query) => {
+  container.innerHTML = `
+  <div class="loader"><h2>Searching Recipes....</h2>
+  <div><img src="./load.gif" width = "152px"></div></div>`;
+
+  try {
+    const data = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`
     );
-    const areaResponse = await areaData.json();
+    const response = await data.json();
 
     container.innerHTML = "";
-
-    areaResponse.meals.forEach((meal) => {
+    response.meals.forEach((meal) => {
       const card = document.createElement("div");
       card.classList.add("recipe-card");
 
@@ -125,96 +116,91 @@ links.forEach((link) => {
         details.style.display = "block";
         container.style.filter = "blur(3px)";
 
-        const mealId = meal.idMeal;
-        console.log(mealId);
+        const youtubeId = meal.strYoutube.slice(32, meal.strYoutube.length);
 
-        idDataLookup(mealId);
+        content.innerHTML = `
+  
+        <h2>${meal.strMeal}</h2>
+        <h3>Ingredients</h3>
+        <ul>${fetchIngredients(meal)}</ul>
+        <h3>Instructions</h3>
+        <p>${meal.strInstructions}</p>
+        <h3>Video Instructions</h3>
+        
+        <iframe height="250" width="350"
+              src="https://www.youtube.com/embed/${youtubeId}">
+        </iframe>`;
       });
     });
-  });
-});
-
-const idDataLookup = async (id) => {
-  const idData = await fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
-  );
-  const idResponse = await idData.json();
-  console.log(idResponse.meals[0].strMeal);
-  const youtubeId = idResponse.meals[0].strYoutube.slice(
-    32,
-    idResponse.meals[0].strYoutube.length
-  );
-
-  content.innerHTML = `
-
-  <h2>${idResponse.meals[0].strMeal}</h2>
-  <h3>Ingredients</h3>
-  <ul><li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li>
-  <li>abcdefgkfi</li></ul>
-  <h3>Instructions</h3>
-  <p>${idResponse.meals[0].strInstructions}</p>
-  <h3>Video Instructions</h3>
-  
-  <iframe height="250" width="350"
-        src="https://www.youtube.com/embed/${youtubeId}">
-  </iframe>`;
+  } catch (error) {
+    container.innerHTML = `<div class="error"><h2> Error in fetching recipes...</h2> <img src="./error.png"  alt="404-image" width="50%"/></div>`;
+  }
 };
 
-// indianLink.addEventListener("click", () => {
-//   links.forEach((link) => {
-//     const areaName = link.textContent;
-//     link.addEventListener("click", async (e) => {
-//       e.preventDefault();
-//       container.innerHTML = `
-//       <div class="loader"><h2>Searching Recipes....</h2>
-//       <div><img src="./load.gif" width = "152px"></div></div>
+//  search recipe by Id function################################
+const idDataLookup = async (id) => {
+  try {
+    const idData = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`
+    );
+    const idResponse = await idData.json();
+    const responseId = idResponse.meals[0];
 
-//     `;
-//       const areaData = await fetch(
-//         `https://www.themealdb.com/api/json/v1/1/filter.php?a=${areaName}`
-//       );
-//       const areaResponse = await areaData.json();
+    const youtubeId = responseId.strYoutube.slice(
+      32,
+      responseId.strYoutube.length
+    );
 
-//       container.innerHTML = "";
+    content.innerHTML = `
+  
+    <h2>${responseId.strMeal}</h2>
+    <h3>Ingredients</h3>
+    <ul>${fetchIngredients2(responseId)}</ul>
+    <h3>Instructions</h3>
+    <p>${responseId.strInstructions}</p>
+    <h3>Video Instructions</h3>
+    
+    <iframe class="video"
+          src="https://www.youtube.com/embed/${youtubeId}">
+    </iframe>`;
+  } catch (error) {
+    container.innerHTML = `<div class="error"><h2> Error in fetching recipes...</h2> <img src="./error.png"  alt="404-image" width="50%"/></div>`;
+  }
+};
 
-//       areaResponse.meals.forEach((meal) => {
-//         const card = document.createElement("div");
-//         card.classList.add("recipe-card");
+// Function for recipe by ID ingredient list ####################
 
-//         card.innerHTML = `
-//         <img src="${meal.strMealThumb}" alt="${meal.strArea}" />
-//         <h3>${meal.strMeal}</h3>
-//         <h5><span>${meal.strArea}</span> Dish</h5>
-//         <h5>Belongs to <span>${meal.strCategory}</span> Category</h5>`;
+const fetchIngredients2 = (responseId) => {
+  let ingredientsList = "";
 
-//         container.appendChild(card);
+  for (i = 1; i <= 20; i++) {
+    let ingredient = responseId[`strIngredient${i}`];
+    if (ingredient) {
+      const measure = responseId[`strMeasure${i}`];
+      ingredientsList += `<li>${measure} ${ingredient}</li>`;
+    } else {
+      break;
+    }
+  }
+  return ingredientsList;
+};
 
-//         const viewButton = document.createElement("button");
+// Function for recipeingredient list ####################
 
-//         viewButton.innerText = "View recipe";
-//         card.appendChild(viewButton);
+const fetchIngredients = (meal) => {
+  let ingredientsList = "";
 
-//         viewButton.addEventListener("click", () => {
-//           details.style.display = "block";
-//           container.style.filter = "blur(3px)";
-
-//           const mealId = meal.idMeal;
-//           console.log(mealId);
-
-//           idDataLookup(mealId);
-//         });
-//       });
-//     });
-//   });
-// });
+  for (i = 1; i <= 20; i++) {
+    let ingredient = meal[`strIngredient${i}`];
+    if (ingredient) {
+      const measure = meal[`strMeasure${i}`];
+      ingredientsList += `<li>${measure} ${ingredient}</li>`;
+    } else {
+      break;
+    }
+  }
+  return ingredientsList;
+};
 
 //  #############################################################
 
